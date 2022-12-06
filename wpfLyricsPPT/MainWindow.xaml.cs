c1be230aeb2a806093d8ab4c4d405fa26bc5e787
Hei-dev <anpromer71@gmail.com>
2022-12-06 08:58:46 +0800

    Add project files.

---
+++ wpfLyricsPPT/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+//https://learn.microsoft.com/en-us/dotnet/desktop/wpf/introduction-to-wpf?view=netframeworkdesktop-4.8&preserve-view=true
+
+namespace wpfLyricsPPT
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btn_addSection_onClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn_selImg_onClick(object sender, RoutedEventArgs e)
+		{
+			//From https://learn.microsoft.com/en-us/dotnet/api/microsoft.win32.openfiledialog?view=windowsdesktop-6.0
+			// Configure open file dialog box
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+			dlg.Filter = "All Supported Image files|*.jpg;*.jpeg;*.png" +
+			 "|JPEG Images|*.jpg;*.jpeg" +
+			 "|PNG Images|*.png" +
+			 "|All Files|*.*";
+
+			// Show open file dialog box
+			Nullable<bool> result = dlg.ShowDialog();
+
+			// Process open file dialog box results
+			if (result == true)
+			{
+				img_preview.Source = new ImageSourceConverter().ConvertFromString(dlg.FileName) as ImageSource;// dlg.FileName;
+				lbl_imgName.Content = "Background Image Preview: " + dlg.FileName;
+			}
+		}
+	}
+}
---
+++ wpfLyricsPPT/lyPptx.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using pptApplication = Microsoft.Office.Interop.PowerPoint.Application;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+public class lyPptx
+{
+	public lyPptx()
+	{
+
+	}
+
+	public static void addToSlide(Slides slides, int idx, CustomLayout customLayout, String text, String typeface, float size, Color fontColor, Color glowColor)
+	{
+		_Slide newSlide = slides.AddSlide(idx, customLayout);
+		//Add text
+		//Using TextRange2 to support Glow effect
+		TextRange2 titleBox = newSlide.Shapes[1].TextFrame2.TextRange;
+		titleBox.Text = text;
+		titleBox.Font.Name = typeface;
+		titleBox.Font.Size = size;
+		titleBox.Font.Glow.Color.RGB = glowColor.ToArgb();
+		//newSlide.Shapes[1].TextFrame2.TextRange.Font.Color.RGB = fontColor.ToArgb();
+
+		//Use TextRange to support noral font color
+		newSlide.Shapes[1].TextFrame.TextRange.Font.Color.RGB = fontColor.ToArgb();
+
+	}
+
+	//static void Main(string[] args)
+	public static void newPresentation()
+	{
+		pptApplication pptapp = new pptApplication();
+
+		Presentation pptfile = pptapp.Presentations.Add(MsoTriState.msoTrue);
+
+		OpenFileDialog dlg = new OpenFileDialog();
+		dlg.ShowDialog();
+
+		if (dlg.ShowDialog() == DialogResult.OK)
+		{
+			string fileName;
+			fileName = dlg.FileName;
+			MessageBox.Show(fileName);
+		}
+
+		Slides slideObj = pptfile.Slides;
+
+		addToSlide(slideObj, 0, pptfile.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutTitleOnly],"Hello World","Arial",20,Color.FromArgb(128,128,128),Color.Black);
+
+		pptfile.SaveAs("test.pptx");
+	}
+
+	
+}
+
